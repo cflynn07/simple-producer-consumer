8a5ae248a879106ab6f41cfdf8772ecd1bf667ab
Casey Flynn <cflynn.us@gmail.com>
2016-01-25 22:49:03 -0700

    Add unit tests for 100% coverage logger module

---
+++ src/logger_test.js
@@ -0,0 +1,26 @@
+/**
+ * @module src/logger_test
+ */
+'use strict'
+
+const test = require('unit.js')
+
+const logger = require('./logger')
+
+const sinon = test.sinon
+
+describe('src/logger', () => {
+
+  it('load', () => {
+    test.function(logger)
+  })
+
+  describe('logger function', () => {
+    it('should return a child bunyan logger', () => {
+      const log = logger(__filename)
+      test.object(log).match({
+        info: sinon.match.func
+      })
+    })
+  })
+})
